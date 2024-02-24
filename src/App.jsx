9a98f5a155b2578ce00de50905caf3b9bd72150a
Vasilyev-Anton <vasilyev1812@gmail.com>
2024-02-24 20:03:55 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Portfolio from './components/Portfolio';
+import files from './files'
+
+function App() {
+
+  return (
+      <Portfolio  files={files}/>
+  )
+}
+
+export default App
